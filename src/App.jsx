765cd62a8dd23fb179fc83f52849a9007fa97a15
mohit-git1 <mohitsharma2826@gmail.com>
2026-03-13 23:53:31 +0530

    protect list, details, and result routes with ProtectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,24 @@ import Login from './pages/Login'
 import List from './pages/List'
 import Details from './pages/Details'
 import Result from './pages/Result'
+import ProtectedRoute from './components/ProtectedRoute'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/list" element={<List />} />
-      <Route path="/details/:id" element={<Details />} />
-      <Route path="/result" element={<Result />} />
+
+      {/* These routes require login */}
+      <Route path="/list" element={
+        <ProtectedRoute><List /></ProtectedRoute>
+      } />
+      <Route path="/details/:id" element={
+        <ProtectedRoute><Details /></ProtectedRoute>
+      } />
+      <Route path="/result" element={
+        <ProtectedRoute><Result /></ProtectedRoute>
+      } />
     </Routes>
   )
 }
